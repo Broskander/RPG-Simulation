72e0d8055eeb2dd1aa9c30bcd229f5005cd0e527
Ayaan Syed <114111679+Broskander@users.noreply.github.com>
2023-04-08 06:32:47 -0500

    Driver file for code
    Runs the simulated turn using random values assigned to their respective class's abilities.

---
+++ GameDriver.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import asg6sp23.Character;
+import asg6sp23.Warrior;
+import asg6sp23.Mage;
+import asg6sp23.Druid;
+
+
+public class GameDriver {
+	public static void main(String[] args) {
+		
+        Random rand = new Random();  // Allows for a random number generator 
+        
+     // Creates all of the characters respectively
+        Warrior warrior = new Warrior("Guts", rand.nextInt(75) + 1, rand.nextInt(50) + 1, rand.nextInt(75) + 1);
+        Mage mage = new Mage("Harry Potter", rand.nextInt(50) + 1, rand.nextInt(100) + 1, rand.nextInt(75) + 1);
+        Druid druid = new Druid("Malfurion", rand.nextInt(75) + 1, rand.nextInt(50) + 1, rand.nextInt(50) + 1, rand.nextInt(50) + 1);
+        Character skeleton = new Character("Dry Bones", rand.nextInt(25) + 1, rand.nextInt(5) + 1);
+        Character zombie = new Character("Walker", rand.nextInt(30) + 1, rand.nextInt(10) + 1);
+        Character goblin = new Character("Griphook", rand.nextInt(40) + 1, rand.nextInt(35) + 1);
+        
+        // Prints all of the stats before any actions
+        System.out.println(warrior.toString());
+        System.out.println(mage.toString());
+        System.out.println(druid.toString());
+        System.out.println(skeleton.toString());
+        System.out.println(zombie.toString());
+        System.out.println(goblin.toString());
+        
+        System.out.println("\n");
+        
+        // Actions
+        warrior.swordAttack(skeleton);
+        mage.fireballSpell(zombie);
+        goblin.attackCommand(warrior);
+        druid.heal(warrior);
+        druid.swordAttack(goblin);
+        
+        System.out.println("\n");
+        
+        // Print out the stats after actions
+        System.out.println(warrior.toString());
+        System.out.println(mage.toString());
+        System.out.println(druid.toString());
+        System.out.println(skeleton.toString());
+        System.out.println(zombie.toString());
+        System.out.println(goblin.toString());
+        
+	}
+}
